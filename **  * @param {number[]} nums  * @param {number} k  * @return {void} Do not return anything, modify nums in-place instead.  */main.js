10f1292b49c566a78c3cb514db259c4f81fb601d
kenneth-lws <wslamal@connect.ust.hk>
2022-03-22 13:23:05 +0800

    Create main.js

---
+++ **  * @param {number[]} nums  * @param {number} k  * @return {void} Do not return anything, modify nums in-place instead.  */main.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let res= nums.slice();
+    for(let i=0;i<nums.length;i++){
+       nums[(i+k)%nums.length]= res[i]
+    }
+};
